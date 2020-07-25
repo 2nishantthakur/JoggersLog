bb70fa66ac5f2b20100407d3334f750adc8d8dc2
Nishant <2nishantthakur@gmail.com>
2020-07-25 22:36:00 +0530

    add launchScreenStoryboard and design 1st view

--- Joggers Log/ViewController.swift
+++ Joggers Log/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var logIn: UIButton!
+    @IBOutlet var signUp: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        logIn.layer.borderWidth = 1
+        logIn.layer.borderColor = UIColor.white.cgColor
+        logIn.layer.cornerRadius = logIn.frame.height/2
+        signUp.layer.borderWidth = 1
+        signUp.layer.borderColor = UIColor.white.cgColor
+        signUp.layer.cornerRadius = logIn.frame.height/2
         // Do any additional setup after loading the view.
     }
 
